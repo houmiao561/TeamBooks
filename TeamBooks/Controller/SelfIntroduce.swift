6e35405f377b4e0f681933a83a4b79142f8db1a7
侯淼 <houmiao@houmiaodeMacBook-Air.local>
2023-10-15 11:46:33 +0800

    Version 2.3 Succeed

--- TeamBooks/Controller/SelfIntroduce.swift
+++ TeamBooks/Controller/SelfIntroduce.swift
@@ -6,30 +6,24 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class SelfIntroduce: UIViewController {
 
     var teamName123 = ""
     var oneselfUID123 = ""
+    let ref = Database.database().reference()
+    var sendMessage = ["":""]
+    let user = Auth.auth().currentUser!
     
     @IBOutlet weak var teamName: UITextField!
     @IBOutlet weak var oneselfUID: UITextField!
     
     @IBOutlet weak var oneselfIntroduce: UITextField!
-    
     @IBOutlet weak var oneselfJob: UITextField!
-    
     @IBOutlet weak var oneselfBirthday: UITextField!
-    
     @IBOutlet weak var oneselfName: UITextField!
-    
-    @IBOutlet weak var oneselfGesture: UITextField!
-    
-    
-    
-    
-    
-    
     
     
     
@@ -39,6 +33,25 @@ class SelfIntroduce: UIViewController {
         oneselfUID.text = oneselfUID123
     }
     
+    @IBAction func add(_ sender: UIButton) {
+        sendMessage = ["teamName":teamName.text!,
+                           "oneselfUID":oneselfUID.text!,
+                           "oneselfIntroduce":oneselfIntroduce.text!,
+                           "oneselfJob":oneselfJob.text!,
+                           "oneselfBirthday":oneselfBirthday.text!,
+                           "oneselfName":oneselfName.text!]
+        
+        ref.child("OneselfIntroduceInTeam").child("\(teamName.text!)").child("Members \(user.uid)").updateChildValues(sendMessage) { (error, _) in
+            if let error = error {
+                print("Error saving data: \(error.localizedDescription)")
+                
+            } else {
+                print("Data successfully saved")
+                self.dismiss(animated: true)
+            }
+        }
+        
+    }
     
 
 }
